686becb1f0954c297c2c56493619e698ddb84421
Alper Yazagan <alperyazagan@gmail.com>
2023-03-15 15:35:41 +0300

    change3

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
-// Write your code here!
-const element = document.createElement("div");
-document.body.append(element);
+const testVar = "hi";
 
-const ul = document.createElement("ul");
+/* Enter the code to remove the node element here */
+document.querySelector("main#main").remove();
 
-for (let i = 0; i < 3; i++) {
-  const li = document.createElement("li");
-  li.textContent = (i + 1).toString();
-  ul.append(li);
-}
-
-element.append(ul);
+/* Create your new element here and assign it to newHeader */
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.innerHTML = "Flatiron School is the champion!";
+newHeader.className = "victory";
